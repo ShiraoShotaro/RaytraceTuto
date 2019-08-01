c16b126ffbe033ce4734065fa50633f8ef7cc0f4
Shirao Shotaro <shirao.shotaro@gmail.com>
2019-08-01 00:27:13 -0700

    add Doxygen comment and delete 'pragma once'

--- src/Constant.hpp
+++ src/Constant.hpp
@@ -1,12 +1,16 @@
 ﻿#ifndef WLIB_CONSTANT_HPP
 #define WLIB_CONSTANT_HPP
-#pragma once
 
 namespace wlib {
 namespace constant {
-constexpr double kPI = 3.14159265358979323846;
-constexpr double kInf = 1e128;
-constexpr double kEps = 1e-6;
+/// @brief PI number.
+constexpr float kPI = 3.14159265358979f;
+
+/// @brief Infinity number.
+constexpr float kInf = 1e32f;
+
+/// @brief Delta number.
+constexpr float kEps = 1e-6f;
 }
 }
 
